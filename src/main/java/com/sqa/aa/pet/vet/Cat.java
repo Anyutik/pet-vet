cd0cf6cb854b6523b4aed1017f9e1529b0ec72af
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:53:04 -0800

    Create Cat Subclass of Mammal

---
+++ src/main/java/com/sqa/aa/pet/vet/Cat.java
@@ -0,0 +1,29 @@
+/**
+ *   File Name: Cat.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Cat //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Cat extends Mammal {
+	private boolean killingMice;
+	private boolean claws;
+
+}
