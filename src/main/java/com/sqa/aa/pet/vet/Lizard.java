41d63e7f096cff17f6349e04e9e12e2da47ef2b0
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:54:36 -0800

    Create Lizard Subclass of Reptile

---
+++ src/main/java/com/sqa/aa/pet/vet/Lizard.java
@@ -0,0 +1,29 @@
+/**
+ *   File Name: Lizard.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Lizard //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Lizard extends Reptile {
+	private boolean lostTail;
+	private boolean changeColor;
+
+}
