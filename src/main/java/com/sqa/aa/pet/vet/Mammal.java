2950cf0afbeb37e2b64d89ebe48dc46365f41ad4
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:48:38 -0800

    Create Mammals Subclasses

---
+++ src/main/java/com/sqa/aa/pet/vet/Mammal.java
@@ -0,0 +1,29 @@
+/**
+ *   File Name: Mammal.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Mammal //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Mammal extends Pet {
+	private boolean rabiesShot;
+	private boolean houseBroken;
+
+}
