f58cf92de32c6fcfb5f54dc3e88db5cd331361e7
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:53:47 -0800

    Create Dog Subclass of Mammal

---
+++ src/main/java/com/sqa/aa/pet/vet/Dog.java
@@ -0,0 +1,30 @@
+/**
+ *   File Name: Dog.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Dog //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Dog extends Mammal {
+	private boolean performsAtShows;
+	private boolean wolfBreed;
+	private string serviceType;
+
+}
