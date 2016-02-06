0f3ac2e79ab1171cf6912d09058f6af56d6fc678
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:55:14 -0800

    Create Snale Subclass of Reptile

---
+++ src/main/java/com/sqa/aa/pet/vet/Snake.java
@@ -0,0 +1,28 @@
+/**
+ *   File Name: Snake.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Snake //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Snake extends Reptile {
+	private int shedFrequency;
+
+}
