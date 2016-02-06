9d9c623be72d448079cd8b0ba49edd77c3a42b15
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 15:50:14 -0800

    Create Reptile Subclasses

---
+++ src/main/java/com/sqa/aa/pet/vet/Reptile.java
@@ -0,0 +1,30 @@
+/**
+ *   File Name: Reptile.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Reptile //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Reptile {
+	private boolean scales;
+	private int heatLevel;
+	private boolean poinsonous;
+
+}
