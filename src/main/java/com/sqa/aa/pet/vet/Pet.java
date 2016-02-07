0957b4aa13e0b58cc9e1c226e7ec411d8a29021d
Anna Antonyan <annaantonyan@annas-air-2.gateway.pace.com>
2016-02-06 16:04:17 -0800

    Add Pet Class

---
+++ src/main/java/com/sqa/aa/pet/vet/Pet.java
@@ -0,0 +1,36 @@
+/**
+ *   File Name: Pet.java<br>
+ *
+ *   Antonyan, Anna<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Feb 6, 2016
+ *   
+ */
+
+package com.sqa.aa.pet.vet;
+
+/**
+ * Pet //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author LastName, FirstName
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class Pet {
+	private boolean Alive;
+	private String name;
+	private boolean injured;
+	private boolean male;
+	private String breed;
+
+}
+// @Override
+// public String toString() {
+// return "<<" + this.getClass().getSimpleName() + ">>";
+// }
